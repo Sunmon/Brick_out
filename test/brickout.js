812e251fea495401c2034c50804b2477888cdc6d
Sunmon <blackmilk274@gmail.com>
2019-05-08 15:14:45 +0900

    add testing

--- test/brickout.js
+++ test/brickout.js
@@ -4,29 +4,29 @@ var context;
 var block_width;
 var block_height;
 var ball;
+var blocks; //블럭 배열
+const max_blocks_in_row = 15;
 // window load시 실행될 함수
 window.onload = function(){
     init();
-    initStage();
-ball = new BallClass(100,100);
-
-    drawBall(ball);
+    initMap();
+    // initStage();
 }
 
 
-//init stage
+//전체 게임 초기화
 function init()
 {
     canvas = document.getElementById("frame");
     context = canvas.getContext("2d");
-    block_width = canvas.width / 15;
+    block_width = canvas.width / max_blocks_in_row;
     block_height = 10;
 }
 
 
 // Block class
 // https://infoscis.github.io/2018/02/13/ecmascript-6-introducing-javascript-classes/
-class BlockClass
+class Block
 {
     // 블럭 생성자
     constructor(x, y, color) {
@@ -42,14 +42,30 @@ class BlockClass
 //블럭 객체 생성
 function createBlock(x, y, color)
 {
-    return  new BlockClass(x,y,color);
+    return  new Block(x,y,color);
 }
 
-// 블럭 그리기
-function drawBlock(block)
+// 블럭 배열 입력받아서 그리기
+function drawBlocks()
 {
-    context.strokeStyle = block.color;
-    context.strokeRect(block.x, block.y, block.width, block.height);
+
+    blocks.forEach(blockRow =>
+    {
+    document.write("block Row : " + blockRow.x+ blockRow.length + "<br/>");
+
+        blockRow.forEach(block=>
+            {
+                document.write(block.length);
+                document.write(block);
+                document.write("<br/>block col <br/>");
+
+                // if(!block.status) return;
+                console.log(block.x, block.y);
+                context.strokeStyle = block.color;
+                context.strokeRect(block.x, block.y, block.width, block.height);
+            });
+        
+    });
 }
 
 // init stage
@@ -61,6 +77,7 @@ function initStage()
     initTimer();
 }
 
+
 function initScore()
 {
     //점수 초기화
@@ -71,30 +88,60 @@ function initTimer()
     //타이머 초기화
 }
 
-// 맵 그리기. 스테이지별로 다른 맵 하려면 캡슐화 이용해야 함. 
-function initMap()
+
+
+// num_block개씩 line줄만큼 블럭 생성
+function makeBlocks(lines, num_blocks)
 {
     // 임시적으로 맵 하나라고 가정.
     var color = ["red", "orange", "yellow", "green", "blue"];
+    var colorIndex = 0;
 
-    // TODO: block 2차원 배열 생성
-    // var blocks = 이차원배열();
-
-    // TODO: Blocks 그리기
-    for(var j = 0; j< 5; j++)
-    {
-        for(var i = 0; i<15 - j*3; i++)
+    blocks = Array.from(new Array(lines));
+    blocks.forEach(block=>
         {
-            var block = createBlock(i*block_width, j*block_height, color[j]);
-            drawBlock(block);
-        }
-    }
+            for(var i = 0; i<num_blocks; i++)
+                block.push(createBlock(0,0,"red"));
+        });
+
+
+}
+
+// 맵 그리기. 스테이지별로 다른 맵 하려면 캡슐화 이용해야 함. 
+function initMap()
+{
+    makeBlocks(5,max_blocks_in_row);
+
+
+    document.write(blocks.length + "<br/>");
+    document.write(blocks[0]);
+    document.write(blocks[1]);
+
+
+    document.write(blocks[0].length);
+
+/*     blocks.forEach(line=>
+        {
+            line.forEach(block=>
+                {
+                   block = createBlock(0,0,"red");
+                   document.write(block.color + "<br/>");
+                });
+        }); */
+
+    document.write(blocks[0][1].color);
+
+
+
 }
 
 
 
 
-class BallClass
+
+
+
+class Ball
 {
      // 공 생성자
      constructor(x, y) {
@@ -139,9 +186,9 @@ function collisionDetection(ball)
 }
 
 // draw
-function draw()
+function draw(ball)
 {
+    drawBlocks();
     drawBall(ball);
     // var update = setInterval(updateBall, 50);
 }
-
